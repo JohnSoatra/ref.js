c181205cb45916e78e6f694d8120360f7a84b528
soatra <soatra206.infinity@gmail.com>
2025-11-10 11:20:15 +0700

    config rollback to not include .map files in dist folder for prod

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,24 @@ import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import dts from "rollup-plugin-dts";
 
+// Get environment variable
+const isProd = process.env.env === "prod";
+
 export default [
   // JS bundle
   {
     input: "src/index.ts",
     output: [
-      { file: "dist/index.cjs.cjs", format: "cjs", sourcemap: true }, // CJS
-      { file: "dist/index.esm.js", format: "esm", sourcemap: true },  // ESM
+      {
+        file: "dist/index.cjs.cjs",
+        format: "cjs",
+        sourcemap: !isProd,
+      }, // CJS
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+        sourcemap: !isProd,
+      }, // ESM
     ],
     plugins: [
       resolve(),
@@ -23,8 +34,6 @@ export default [
   {
     input: "src/index.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [
-      dts(),
-    ],
+    plugins: [dts()],
   },
 ];
