96925e48b848ac7b5e503955fd43d1e0ad116a7b
soatra <soatra206.infinity@gmail.com>
2025-11-11 17:59:18 +0700

    playgroup index.ts

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,10 +1,11 @@
 import ref, { ChangeEvent } from '../../../vref';
 
-const proxy = ref([{name: 'nano'}], (evt) => {
+const proxy = ref(new Set([1]), (evt) => {
   console.log('on change ', evt);
 })
 
-proxy.value.push({name: 'sdf'  });
-Object.defineProperty(proxy.value, 'test', {value: 34234 })
+// proxy.value.fill(1);
+proxy.value.add.apply(new Set([1]), [1])
+// Object.defineProperty(proxy.value, 'test', {value: 34234 })
 
-console.log(proxy.value);
+// console.log(proxy.value);
